b644bba0cae45610ef4a3c1b647fdbcf79ebaffa
David Tolnay <dtolnay@gmail.com>
2025-12-21 10:03:50 -0800

    Release 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@
 //! ![performance](https://raw.githubusercontent.com/dtolnay/zmij/master/performance.png)
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/zmij/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/zmij/0.1.1")]
 #![deny(unsafe_op_in_unsafe_fn)]
 #![allow(
     clippy::blocks_in_conditions,
